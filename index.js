40be0f9112fb29b8dfe15287a3e43014993bd98d
Ariful2634 <80531678+Ariful2634@users.noreply.github.com>
2023-10-19 17:01:40 +0600

    getting data from the database

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ app.use(cors())
 app.use(express.json())
 
 
+
+
+
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.stv3jdc.mongodb.net/?retryWrites=true&w=majority`;
 
-
+// console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,12 +27,29 @@ const client = new MongoClient(uri, {
   }
 });
 
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
     const techCollection = client.db("techDB").collection("tech")
+
+    // read
+
+    app.get('/tech', async(req,res)=>{
+        const cursor = techCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+
+    // app.get('/tech/:name',async(req,res)=>{
+    //     const id=req.params.id;
+    //     const query = {_id: new ObjectId(id)}
+    //     const result = await techCollection.findOne(query);
+    //     res.send(result)
+    // })
 
     // create
 
