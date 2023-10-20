acd9d6fdc8fd727b239459caef65733b17301fdd
Ariful2634 <80531678+Ariful2634@users.noreply.github.com>
2023-10-20 22:43:41 +0600

    finalized project

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const techCollection = client.db("techDB").collection("tech")
     const cartCollection = client.db("techDB").collection("cart")
@@ -107,7 +107,7 @@ async function run() {
 
     app.delete('/cart/:id', async(req,res)=>{
         const id = req.params.id;
-        const query = {_id: id}
+        const query = {_id: new ObjectId(id)}
         const result = await cartCollection.deleteOne(query)
         res.send(result)
     })
@@ -116,7 +116,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
